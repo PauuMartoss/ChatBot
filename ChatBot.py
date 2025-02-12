7ac303fe804a1018cc63b00d3a792279d0a96e09
Pau <paumartos2009@gmail.com>
2025-02-12 18:47:05 +0100

    commit2

---
+++ ChatBot.py
@@ -0,0 +1,5 @@
+import streamlit as st
+with st.chat_message("assistant", avatar="🤖"):
+    st.write("Hola soy una IA")
+with st.chat_message("user", avatar="🧑🏻"):
+    st.write("Hola soy un humano")
