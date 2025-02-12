35e9c76cce1db878462d61b89583936b8d3d26f1
Pau <paumartos2009@gmail.com>
2025-02-12 18:49:17 +0100

    commit3

--- ChatBot.py
+++ ChatBot.py
@@ -1,5 +1,8 @@
 import streamlit as st
+st.title ("PauGPT")
 with st.chat_message("assistant", avatar="🤖"):
     st.write("Hola soy una IA")
 with st.chat_message("user", avatar="🧑🏻"):
     st.write("Hola soy un humano")
+
+mensaje_usuario = st.chat_input("Escribe un mensaje")
